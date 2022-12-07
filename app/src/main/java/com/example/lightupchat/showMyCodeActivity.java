9ddbaaede8d865199587f25b418c0eeb2c9d97c8
Tun Win Lat <tunwinlatt@windowslive.com>
2022-12-06 17:16:07 -0800

    Fixed errors.

--- app/src/main/java/com/example/lightupchat/showMyCodeActivity.java
+++ app/src/main/java/com/example/lightupchat/showMyCodeActivity.java
@@ -6,13 +6,15 @@ import android.widget.TextView;
 
 public class showMyCodeActivity extends AppCompatActivity {
 
+    TextView code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showmycode);
 
-
+        code = findViewById(R.id.codeLable);
+        code.setText(AuthenticationActivity.getShowID());
 
 
     }
